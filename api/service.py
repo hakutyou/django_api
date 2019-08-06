0608afd81f2d5f0f0624f9ade72b2ad2322bfaaa
siratori <siratori@qq.com>
2019-08-06 14:09:59 +0800

    celery 测试

--- api/service.py
+++ api/service.py
@@ -1,10 +1,21 @@
 import logging
+import os
+
+from celery import Celery
+from django.conf import settings
 
 # Logger
 logger = logging.getLogger(__name__)
 
 # Celery
-from django.conf import settings
-from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+app = Celery('api', broker=settings.CELERY_BROKER, backend=settings.CELERY_BACKEND)
 
-app = Celery('my_task', broker=settings.CELERY_BROKER, backend=settings.CELERY_BACKEND)
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
--- excel/controller/reader.py
+++ excel/controller/reader.py
@@ -3,9 +3,9 @@ import time
 from api.service import app
 
 
-@app.task
+@app.task(bind=True)
 # TODO: celery 测试
-def add(x, y):
+def add(self, x, y):
     print('enter call function ...')
     time.sleep(5)
     return x + y
