2ec233de46fbf551dca80f12c7363c458e9a481e
lihu <lihu@emilia.fun>
2019-10-28 11:36:02 +0800

    celery 和 django 共用 venv

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+. venv/bin/activate
 pip install -r requirements.txt
 mkdir log -p
 cp script/product.env .env
 
-watchmedo auto-restart -p './requirements.txt' -- ./install.sh &
+# watchmedo auto-restart -p './requirements.txt' -- ./install.sh &
 gunicorn -c gunicorn.py api.wsgi --capture-output
 
