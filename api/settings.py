b1a2418bf61a5f6ab721b667307b826938e5591c
lihu <lihu@emilia.fun>
2019-11-19 14:34:40 +0800

    celery 添加密码认证

--- api/settings.py
+++ api/settings.py
@@ -211,8 +211,8 @@ CACHES = {
 }
 
 # 7.1 celery 配置
-CELERY_BROKER = f'redis://{config("REDIS_HOST")}:{config("REDIS_PORT")}/{config("BROKER_INDEX")}'
-CELERY_BACKEND = f'redis://{config("REDIS_HOST")}:{config("REDIS_PORT")}/{config("BACKEND_INDEX")}'
+CELERY_BROKER = f'redis://:{config("REDIS_PASSWORD")}@{config("REDIS_HOST")}:{config("REDIS_PORT")}/{config("BROKER_INDEX")}'
+CELERY_BACKEND = f'redis://:{config("REDIS_PASSWORD")}@{config("REDIS_HOST")}:{config("REDIS_PORT")}/{config("BACKEND_INDEX")}'
 CELERY_IMPORTS = (
     'excel.controller',
     'external.interface',
