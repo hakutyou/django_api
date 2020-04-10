82ef369830bf877c6eb0a211f8f0688994bfa8cf
hakutyou <lihu@emilia.fun>
2020-04-10 15:05:42 +0800

    proxy_common 支持 get 方式

--- external/interface/scf.py
+++ external/interface/scf.py
@@ -1,8 +1,8 @@
 import requests
+from django.views.decorators.csrf import csrf_exempt
 
 from api.shortcuts import Response, request_check
 from external.service import scf_service
-from django.views.decorators.csrf import csrf_exempt
 
 
 def scf_common(request, path):
@@ -17,13 +17,22 @@ def scf_common(request, path):
 @csrf_exempt
 @request_check(
     url=(str, True),
-    json=(str, False)
+    json=(str, False),
+    method=(str, False)
 )
 def proxy_common(request):
     """
     转外网调用，私有网络（SCF）访问外网时使用
     """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/55.0.2883.87 Safari/537.36'
+    }
     url = request.post.get('url')
     json = request.post.get('json')
-    response = requests.post(url, json=json)
+    method = request.post.get('method')
+    if method == 'get':
+        response = requests.get(url, json=json, headers=headers)
+    else:
+        response = requests.post(url, json=json, headers=headers)
     return Response(request, 0, _type='str', data=response.text)
